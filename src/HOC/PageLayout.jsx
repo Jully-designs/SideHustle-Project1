8dcf7a163b09e3a4cb520196f73fbe091da36e84
Juliet <olamidehorlly@gmail.com>
2021-11-13 09:37:08 +0100

    commit on 'master'

---
+++ src/HOC/PageLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageLayout = ({children}) => {
+    return(
+        <div>
+            <h1>HOC</h1>
+            {children}
+        </div>
+    )
+}
+
+export default PageLayout;
